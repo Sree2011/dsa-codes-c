d2e6d2212dfb1bdee0f9aecb943fb4f501ed9197
Sree2011 <sreesainandinigundraju@gmail.com>
2023-08-30 22:13:28 +0530

    modified:   stacks/stack_definition.py

--- stacks/stack_definition.py
+++ stacks/stack_definition.py
@@ -11,20 +11,28 @@ class Stack_python:
         else:
             self.top +=1
             self.stack[self.top] = item
-    
+            
+
     def topele(self):
         return self.stack[self.top]
     
     def isEmpty(self):
-        if(self.top < 1):
+        if(not all(self.stack)):
             return True
         else:
             return False
+
+    def display(self):
+        print("Stack is : ",end="")
+        print(self.stack)
+        
     def size(self):
         return len(self.stack)
 
 d1 = Stack_python()
+
+for i in range (1,11):
+   d1.push(i)
 print("Is stack empty:", d1.isEmpty())
-for i in range (5):
-    d1.push(i)
+d1.display()
 print('Top element:', d1.topele(), 'Size of the stack', d1.size())
